f64cea622ca7725defc1307496e92adbda4aaeb4
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-02-20 03:10:19 +0000

    Add files via upload

--- main.ps1
+++ main.ps1
@@ -6,9 +6,43 @@
 . .\scripts\cleaner.ps1
 
 # Global variables
-$global:FilePath_c2l = ""
+$global:FilePathName_c2l = ""
 $global:ScriptType_x6s = ""
 $global:CurrentContent_o4s = ""
+
+# Global Dictionaries
+$global:CommentMap_k6s = @{
+    "Python" = "#"
+    "PowerShell" = "#"
+    "MQL5" = "//"
+    "Batch" = "REM"
+}
+$global:SectionMap_d8f = @{
+    "Python" = @{
+        "import" = @('^import\s+\w+', '^from\s+\w+\s+import\s+\w+')
+        "variable" = @('^\w+\s*=\s*.+')
+        "dictionary" = @('^[a-zA-Z_]+ = \[', '^[a-zA-Z_]+ = {')
+        "function" = @('^def\s+\w+\(.*\):')
+    }
+    "PowerShell" = @{
+        "import" = @('^Import-Module\s+\w+', '^\.\s+\.\\[a-zA-Z0-9_\-]+\.ps1')
+        "variable" = @('^\$\w+', '`$global:\w+')
+        "dictionary" = @('`$global:(\w+)\s*=\s*@{')
+        "function" = @('^function\s+[a-zA-Z_][a-zA-Z0-9_]*', '^function\s+[a-zA-Z_][a-zA-Z0-9_]*\s*{')
+    }
+    "Batch" = @{
+        "import" = @('^REM IMPORT \w+')
+        "variable" = @('^set ')
+        "dictionary" = @('^REM MAP .+')
+        "function" = @('^:[a-zA-Z_][a-zA-Z0-9_]*', '^if .*\(', '^for .*\(')
+    }
+    "MQL5" = @{
+        "import" = @('^import\s+\w+')
+        "variable" = @('^\s*input\s+(int|double|string|ENUM_TIMEFRAMES)\s+\w+\s*=', '^\s*(int|double|string)\s+\w+\s*=')
+        "dictionary" = @('^double\[\]\s+\w+;', '^int\[\]\s+\w+;', 'string\[\]\s+\w+;')
+        "function" = @('^(int|double|string|void|long|bool)\s+[a-zA-Z_][a-zA-Z0-9_]*\(')
+    }
+}
 
 # Initialize program
 function script-InitializationCode {
